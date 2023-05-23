4b50625500e75f8b9b4985c5820a2bf00824a5c1
Danny Tatom <its.danny@hey.com>
2023-05-23 12:58:06 -0700

    feat(spatial): add map transitions

--- server/src/spatial/plugin.rs
+++ server/src/spatial/plugin.rs
@@ -1,14 +1,23 @@
 use bevy::prelude::*;
 
-use super::commands::*;
+use super::{
+    commands::*,
+    components::{Position, Tile, Transition, Zone},
+};
 
 pub struct SpatialPlugin;
 
 impl Plugin for SpatialPlugin {
     fn build(&self, app: &mut App) {
-        app.add_system(look);
-        app.add_system(map);
-        app.add_system(movement);
-        app.add_system(teleport);
+        app.register_type::<Vec<String>>()
+            .register_type::<Position>()
+            .register_type::<Zone>()
+            .register_type::<Tile>()
+            .register_type::<Transition>()
+            .add_system(look)
+            .add_system(map)
+            .add_system(movement)
+            .add_system(enter)
+            .add_system(teleport);
     }
 }
