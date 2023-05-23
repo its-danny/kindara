47b7ddab81a6c1b16f30563dfdea28b37be4d40f
Danny Tatom <its.danny@hey.com>
2023-05-23 13:47:47 -0700

    refactor: move direction offset into its own function

--- server/src/spatial/utils.rs
+++ server/src/spatial/utils.rs
@@ -1,6 +1,24 @@
+use bevy::prelude::*;
+
 use crate::visual::components::Sprite;
 
 use super::components::Tile;
+
+pub(super) fn offset_for_direction(direction: &str) -> Option<IVec3> {
+    match direction {
+        "north" | "n" => Some(IVec3::new(0, -1, 0)),
+        "northeast" | "ne" => Some(IVec3::new(1, -1, 0)),
+        "east" | "e" => Some(IVec3::new(1, 0, 0)),
+        "southeast" | "se" => Some(IVec3::new(1, 1, 0)),
+        "south" | "s" => Some(IVec3::new(0, 1, 0)),
+        "southwest" | "sw" => Some(IVec3::new(-1, 1, 0)),
+        "west" | "w" => Some(IVec3::new(-1, 0, 0)),
+        "northwest" | "nw" => Some(IVec3::new(-1, -1, 0)),
+        "up" | "u" => Some(IVec3::new(0, 0, 1)),
+        "down" | "d" => Some(IVec3::new(0, 0, -1)),
+        _ => None,
+    }
+}
 
 pub(super) fn view_for_tile(tile: &Tile, sprite: &Sprite) -> String {
     format!("{} {}\n{}", sprite.character, tile.name, tile.description)
