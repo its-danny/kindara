0e7330bec1f9f7faf45daaaa60c980d497ea3fa3
Danny Tatom <its.danny@hey.com>
2023-06-08 17:00:26 -0700

    feat(spatial): add exits to look response

--- server/src/spatial/commands/look.rs
+++ server/src/spatial/commands/look.rs
@@ -10,7 +10,10 @@ use crate::{
     input::events::{Command, ParsedCommand},
     items::components::Item,
     player::components::{Character, Client, Online},
-    spatial::components::Tile,
+    spatial::{
+        components::{Position, Tile, Zone},
+        utils::offset_for_direction,
+    },
     visual::components::Sprite,
 };
 
@@ -36,11 +39,12 @@ pub fn handle_look(
 }
 
 pub fn look(
+    items: Query<&Item>,
     mut commands: EventReader<ParsedCommand>,
     mut outbox: EventWriter<Outbox>,
     players: Query<(&Client, &Character, &Parent), With<Online>>,
-    tiles: Query<(&Tile, &Sprite, &Children)>,
-    items: Query<&Item>,
+    tiles: Query<(&Tile, &Sprite, &Position, Option<&Children>, &Parent)>,
+    zones: Query<(&Zone, &Children)>,
 ) {
     for command in commands.iter() {
         if let Command::Look = &command.command {
@@ -50,20 +54,27 @@ pub fn look(
                 continue;
             };
 
-            let Ok((tile, sprite, children)) = tiles.get(tile.get()) else {
+            let Ok((tile, sprite, position, siblings, zone)) = tiles.get(tile.get()) else {
                 debug!("Could not get tile: {:?}", tile.get());
 
                 continue;
             };
 
-            let items_line = generate_items_line(children, &items);
+            let Ok((_, zone_tiles)) = zones.get(zone.get()) else {
+                debug!("Could not get zone: {:?}", zone.get());
+
+                continue;
+            };
+
+            let exits = get_exits(position, zone_tiles, &tiles);
+            let items_line = get_items_line(siblings, &items);
 
             let output = if character.config.brief {
-                format!("{} {}", sprite.character, tile.name)
+                format!("{} {}{}", sprite.character, tile.name, exits)
             } else {
                 format!(
-                    "{} {}\n{}{}",
-                    sprite.character, tile.name, tile.description, items_line,
+                    "{} {}{}\n{}{}",
+                    sprite.character, tile.name, exits, tile.description, items_line,
                 )
             };
 
@@ -72,9 +83,35 @@ pub fn look(
     }
 }
 
-fn generate_items_line(children: &[Entity], items: &Query<&Item>) -> String {
-    let item_names: HashMap<String, u16> = children
+fn get_exits(
+    position: &Position,
+    zone_tiles: &Children,
+    tiles: &Query<(&Tile, &Sprite, &Position, Option<&Children>, &Parent)>,
+) -> String {
+    let directions = vec!["n", "ne", "e", "se", "s", "sw", "w", "nw", "u", "d"];
+    let mut exits: Vec<String> = vec![];
+
+    for tile in zone_tiles.iter() {
+        if let Ok((_, _, p, _, _)) = tiles.get(*tile) {
+            directions.iter().for_each(|direction| {
+                if p.0 == position.0 + offset_for_direction(direction).unwrap() {
+                    exits.push(direction.to_uppercase());
+                }
+            });
+        }
+    }
+
+    if exits.is_empty() {
+        return "".into();
+    }
+
+    format!(" [{}]", exits.join(", "))
+}
+
+fn get_items_line(siblings: Option<&Children>, items: &Query<&Item>) -> String {
+    let item_names: HashMap<String, u16> = siblings
         .iter()
+        .flat_map(|children| children.iter())
         .filter_map(|child| items.get(*child).ok())
         .map(|item| item.name_on_ground.clone())
         .fold(HashMap::new(), |mut map, name| {
@@ -161,6 +198,36 @@ mod tests {
         let content = get_message_content(&mut app, client_id);
 
         assert_eq!(content, "x The Void\nA vast, empty void.");
+    }
+
+    #[test]
+    fn has_exits() {
+        let mut app = AppBuilder::new().build();
+        app.add_system(look);
+
+        let zone = ZoneBuilder::new().build(&mut app);
+
+        let tile = TileBuilder::new()
+            .sprite("x")
+            .name("The Void")
+            .description("A vast, empty void.")
+            .position(IVec3::new(0, 0, 0))
+            .build(&mut app, zone);
+        TileBuilder::new()
+            .position(IVec3::new(0, -1, 0))
+            .build(&mut app, zone);
+        TileBuilder::new()
+            .position(IVec3::new(0, 0, 1))
+            .build(&mut app, zone);
+
+        let (client_id, _) = PlayerBuilder::new().tile(tile).build(&mut app);
+
+        send_message(&mut app, client_id, "look");
+        app.update();
+
+        let content = get_message_content(&mut app, client_id);
+
+        assert_eq!(content, "x The Void [N, U]\nA vast, empty void.");
     }
 
     #[test]
