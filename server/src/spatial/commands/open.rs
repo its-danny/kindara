39932146ceb52b947082952c53e125acc5045f0c
Danny Tatom <its.danny@hey.com>
2023-12-17 15:55:04 -0800

    refactor(spatial): clean up open command

--- server/src/spatial/commands/open.rs
+++ server/src/spatial/commands/open.rs
@@ -1,10 +1,11 @@
 use std::sync::OnceLock;
 
 use anyhow::Context;
-use bevy::prelude::*;
+use bevy::{ecs::query::WorldQuery, prelude::*};
 use bevy_mod_sysfail::sysfail;
 use bevy_nest::prelude::*;
 use regex::Regex;
+use thiserror::Error;
 
 use crate::{
     input::events::{Command, ParseError, ParsedCommand},
@@ -30,14 +31,28 @@ pub fn handle_open(content: &str) -> Result<Command, ParseError> {
     }
 }
 
+#[derive(WorldQuery)]
+pub struct DoorQuery {
+    entity: Entity,
+    depiction: &'static Depiction,
+    with_door: With<Door>,
+}
+
+#[derive(WorldQuery)]
+#[world_query(mutable)]
+pub struct DoorMutQuery {
+    entity: Entity,
+    door: &'static mut Door,
+}
+
 #[sysfail(log)]
 pub fn open(
     mut commands: EventReader<ParsedCommand>,
     mut outbox: EventWriter<Outbox>,
     players: Query<(&Client, &Parent), With<Online>>,
     tiles: Query<Option<&Children>, With<Tile>>,
-    doors: Query<(Entity, &Depiction), With<Door>>,
-    mut doors_mut: Query<&mut Door>,
+    doors: Query<DoorQuery>,
+    mut doors_mut: Query<DoorMutQuery>,
 ) -> Result<(), anyhow::Error> {
     for command in commands.iter() {
         if let Command::Open(target) = &command.command {
@@ -48,27 +63,13 @@ pub fn open(
 
             let siblings = tiles.get(tile.get())?;
 
-            let Some(target) = target else {
-                outbox.send_text(client.id, "Open what?");
+            let mut door = match get_door(target, &siblings, &doors, &mut doors_mut) {
+                Ok(door) => door,
+                Err(err) => {
+                    outbox.send_text(client.id, err.to_string());
 
-                continue;
-            };
-
-            let Some((entity, _)) = siblings
-                .iter()
-                .flat_map(|siblings| siblings.iter())
-                .filter_map(|sibling| doors.get(*sibling).ok())
-                .find(|(entity, depiction)| depiction.matches_query(entity, target))
-            else {
-                outbox.send_text(client.id, "You can't open that.");
-
-                continue;
-            };
-
-            let Ok(mut door) = doors_mut.get_mut(entity) else {
-                outbox.send_text(client.id, "You can't open that.");
-
-                continue;
+                    continue;
+                }
             };
 
             door.is_open = true;
@@ -78,6 +79,39 @@ pub fn open(
     }
 
     Ok(())
+}
+
+#[derive(Error, Debug, PartialEq)]
+enum CloseError {
+    #[error("Close what?")]
+    NoTargetProvided,
+    #[error("You don't see a {0} here.")]
+    NotFound(String),
+    #[error("You can't close that.")]
+    NotClosable,
+}
+
+fn get_door<'a>(
+    target: &Option<String>,
+    siblings: &Option<&Children>,
+    doors: &Query<DoorQuery>,
+    doors_mut: &'a mut Query<DoorMutQuery>,
+) -> Result<Mut<'a, Door>, CloseError> {
+    let target = target.as_ref().ok_or(CloseError::NoTargetProvided)?;
+
+    let door = siblings
+        .iter()
+        .flat_map(|siblings| siblings.iter())
+        .filter_map(|sibling| doors.get(*sibling).ok())
+        .find(|door| door.depiction.matches_query(&door.entity, target))
+        .map(|door| door.entity)
+        .ok_or_else(|| CloseError::NotFound(target.to_string()))?;
+
+    let door = doors_mut
+        .get_mut(door)
+        .map_err(|_| CloseError::NotClosable)?;
+
+    Ok(door.door)
 }
 
 #[cfg(test)]
